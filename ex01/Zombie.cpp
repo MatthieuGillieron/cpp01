03256f10cb7bc90a99012cf18ddd345d8ddc81a1
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-09-29 14:24:13 +0200

    add: Function announce + unamed zomblar

--- ex01/Zombie.cpp
+++ ex01/Zombie.cpp
@@ -6,7 +6,17 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:03:58 by mg                #+#    #+#             */
-/*   Updated: 2025/09/29 14:03:58 by mg               ###   ########.fr       */
+/*   Updated: 2025/09/29 14:23:51 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Zombie.hpp"
+
+Zombie::Zombie(void) : name("unamed") {}
+Zombie::Zombie(std::string name) : name(name) {}
+Zombie::~Zombie() { std::cout << "Zombie " << name << "is destroyed" << std::endl; }
+
+
+void	Zombie::announce(void) {
+	std::cout << name << ": BraiiiiiiinnnzzzZ... " << std::endl;
+}
