e0ab6213749d84cfa4a1243eabf7a51430714c12
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-09-29 10:20:05 +0200

    add: Constructor / destuctor on zombie.cpp

--- ex00/Zombie.cpp
+++ ex00/Zombie.cpp
@@ -6,12 +6,19 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/27 11:07:22 by mg                #+#    #+#             */
-/*   Updated: 2025/09/29 09:30:23 by mg               ###   ########.fr       */
+/*   Updated: 2025/09/29 10:19:25 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 #include <iostream>
+
+
+Zombie::Zombie(std::string name) : name(name) {}
+Zombie::~Zombie(void) {
+
+	std::cout << "Zombie" << name << " is destroyed" << std::endl;
+}
 
 void	Zombie::announce() {
 
