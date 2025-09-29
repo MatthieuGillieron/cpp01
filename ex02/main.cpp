b9f5edfc6ad4f752c9faaea12099f161de6c9cc4
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-09-29 16:26:40 +0200

    add: log for check value + adress with classic var, pointer and ref

---
+++ ex02/main.cpp
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 16:10:11 by mg                #+#    #+#             */
+/*   Updated: 2025/09/29 16:25:58 by mg               ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main () {
+
+	std::string var("HI THIS IS BRAIN");
+	std::string* stringPTR = &var;
+	std::string& stringREF = var;
+
+	std::cout << "adress var: " << &var << std::endl ;
+	std::cout << "adress stringPTR: " << &stringPTR << std::endl ;
+	std::cout << "adress stringREF: " << &stringREF << std::endl ;
+
+
+	std::cout << "value var: " << var << std::endl ;
+	std::cout << "value stringPTR: " << *stringPTR << std::endl ;
+	std::cout << "value stringREF: " << stringREF << std::endl ;
+
+	return 0;
+}
+
+
+/*
+
++----------------------+      +-------------------+
+|      var             |      |   stringPTR       |
+| "HI THIS IS BRAIN"   |      | 0x1000            |	
+| @ adresse 0x1000     |      | @ adresse 0x2000  |
++----------------------+      +-------------------+
+           ^
+           |
++-------------------+
+|   stringREF       |  (alias, pas une vraie nouvelle case mémoire)
+| réfère var @0x1000|	(modifie stirngREF -> sa modifie var)
++-------------------+
+
+
+*/
