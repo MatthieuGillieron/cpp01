679977d4f081a4654b7547bc1fe4a31074ceeebc
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-09-30 11:52:16 +0200

    add: Class for humanA, (need to check methode attack)

--- ex03/HumanA.cpp
+++ ex03/HumanA.cpp
@@ -6,7 +6,19 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 10:26:23 by mg                #+#    #+#             */
-/*   Updated: 2025/09/30 10:26:24 by mg               ###   ########.fr       */
+/*   Updated: 2025/09/30 11:51:37 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanA.hpp"
+#include "Weapon.hpp"
+#include <iostream>
+
+HumanA::HumanA(std::string name, Weapon& weapon) : name(name), weapon(weapon) {} 
+HumanA::~HumanA(){}
+
+void	HumanA::attack () {
+
+	std::cout << name << "attacks with their " << weapon.getType() << std::endl;
+
+}
