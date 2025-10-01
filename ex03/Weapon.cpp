ab4f0d2eb41ec949f54c58379617d1cfe6c03d43
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-10-01 10:57:48 +0200

    add: class weapon + methode for weapon

--- ex03/Weapon.cpp
+++ ex03/Weapon.cpp
@@ -6,7 +6,21 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 10:26:38 by mg                #+#    #+#             */
-/*   Updated: 2025/09/30 10:26:39 by mg               ###   ########.fr       */
+/*   Updated: 2025/10/01 10:56:59 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Weapon.hpp"
+#include <iostream>
+
+Weapon::Weapon(std::string type) : type(type) {}
+Weapon::~Weapon() {}
+
+
+const std::string&	Weapon::getType() const {
+	return type;
+}
+
+void	Weapon::setType(std::string newType) {
+	type = newType;
+}
--- ex03/Weapon.hpp
+++ ex03/Weapon.hpp
@@ -1,5 +1,24 @@
 
+#include <iostream>
+
 #ifndef WEAPON_HPP
 #define WEAPON_HPP
+
+class Weapon {
+
+	private:
+
+	std::string type;
+
+	public:
+	Weapon(std::string type);
+	~Weapon();
+
+	const	std::string& getType() const;
+	void	setType(std::string newType);
+
+};
+
+
 
 #endif
