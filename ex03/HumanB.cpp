30c49c82c0bc2109b220b4f36e94c4843dd88cb5
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-10-02 16:11:00 +0200

    add: Methode for set weapons + attack for human B

--- ex03/HumanB.cpp
+++ ex03/HumanB.cpp
@@ -6,7 +6,23 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 10:26:27 by mg                #+#    #+#             */
-/*   Updated: 2025/09/30 10:26:28 by mg               ###   ########.fr       */
+/*   Updated: 2025/10/02 16:08:52 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "HumanB.hpp"
+#include <iostream>
+
+void	HumanB::attack() {
+	if (!weapon)
+		std::cout << name << " has no weapon" << std::endl;
+
+	else {
+		std::cout << name << " attack with their " << weapon->getType() << std::endl;
+	}
+}
+
+void	HumanB::setWeapon(Weapon& weapon) {
+
+	this->weapon = &weapon;
+}
--- ex03/HumanB.hpp
+++ ex03/HumanB.hpp
@@ -1,9 +1,25 @@
 
 #include <iostream>
+#include "Weapon.hpp"
+
 
 #ifndef HUAMANB_HPP
 #define HUAMANB_HPP
 
+class HumanB {
 
+	private:
+
+	std::string name;
+	Weapon* weapon;
+
+	public:
+	HumanB(std::string name);
+	~HumanB();
+
+	void	attack();
+	void setWeapon(Weapon& weapon);
+
+};
 
 #endif
