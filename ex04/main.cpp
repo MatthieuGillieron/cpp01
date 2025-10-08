cdf8ed757704946be426547b625ad1e23f959fcf
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-10-08 11:47:39 +0200

    add: replace s1 to s2

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -6,41 +6,13 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/03 15:01:53 by mg                #+#    #+#             */
-/*   Updated: 2025/10/06 15:48:44 by mg               ###   ########.fr       */
+/*   Updated: 2025/10/08 11:46:09 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <fstream>
 #include <string>
-
-
-// Create a program that takes three parameters in the following order: a filename and
-// two strings, s1 and s2.
-// It must open the file <filename> and copy its content into a new file
-// <filename>.replace, replacing every occurrence of s1 with s2.
-// Using C file manipulation functions is forbidden and will be considered cheating. All
-// the member functions of the class std::string are allowed, except replace. Use them
-// wisely!
-// Of course, handle unexpected inputs and errors. You must create and turn in your
-// own tests to ensure that your program works as expected.
-
-
-//  ./exec <file a lire> <mot a trouver> <mot a remplacer>
-
-
-// [1] ouvrir le fichier test, check si possible sinon message d'erreur
-
-// [2] check si empty -> message d'erreur
-
-// [3] lire le fichier complet + copy
-
-// [4] cree un fihcier ayant le meme nom que le file lu et ajouter [.replace]
-
-// [5] copier le contenu du file de lecture
-
-// [6] check si s1 est dans le fichier si oui s1 = s2
-
 
 // [DOC] :
 //
@@ -49,10 +21,6 @@
 //
 // std::istream::peak() -> examine le char et le gare dans le flux :: peak() == EOF au start c'est que empty voir .empty
 //
-
-
-
-
 
 
 
@@ -74,14 +42,12 @@ int	parseFile(char *file) {
 
 
 
-
-
-int main(int , char **av) {
+int main(int ac, char **av) {
 	
-	// if (ac != 4) {
-	// 	std::cout << "exec: <filename> <s1> <s2>" << std::endl;
-	// 	return 0;
-	// }
+	if (ac != 4) {
+		std::cout << "exec: <filename> <s1> <s2>" << std::endl;
+		return 0;
+	}
 	
 	
 	
@@ -91,11 +57,31 @@ int main(int , char **av) {
 	std::string fileName = av[1];
 	std::ofstream createFile(fileName + ".replace");
 	
+	std::string s1 = av[2];
+	std::string s2 = av[3];
 	std::string line; 
 
-
-	
 	while (std::getline(file, line)) {
+
+		std::string replacedLine;
+		std::string::size_type lastPos = 0;
+		std::string::size_type pos = line.find(s1, lastPos);
+
+
+		while (pos != std::string::npos)
+		{
+			replacedLine.append(line, lastPos, pos - lastPos);
+			replacedLine += s2;
+
+			lastPos = pos + s1.size();
+			pos = line.find(s1, lastPos);
+		}
+		
+		replacedLine.append(line, lastPos, std::string::npos);
+		std::cout << replacedLine;
+		if (!file.eof())
+			std::cout << std::endl;
+
 		createFile << line << "\n";
 	}
 
