34e57c024d5d39f386a31aed6c28b81c0bf2c3f9
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-10-17 14:28:10 +0200

    add: minimalist main

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,7 +6,19 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 11:45:21 by mg                #+#    #+#             */
-/*   Updated: 2025/10/16 11:45:22 by mg               ###   ########.fr       */
+/*   Updated: 2025/10/17 14:25:25 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+
+int main() {
+	Harl harl;
+
+	harl.complain("DEBUG");
+	harl.complain("INFO");
+	harl.complain("WARNING");
+	harl.complain("ERROR");
+
+	return 0;
+}
