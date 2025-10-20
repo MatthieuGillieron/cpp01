d040060836c21e2d310c9a3c1ddf0ed9e20f4a5b
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-10-20 14:43:36 +0200

    add: little parsing for main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,18 +6,25 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 10:38:42 by mg                #+#    #+#             */
-/*   Updated: 2025/09/29 10:42:45 by mg               ###   ########.fr       */
+/*   Updated: 2025/10/20 14:43:17 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int	main () {
+int	main (int ac, char **av) {
 	
-	Zombie *heapZombie = newZombie("heapZ");
-	heapZombie->announce();
-	delete heapZombie;
+	(void)av;
+	
+	if (ac == 1) {
 
-	randomChump("stackZ");
-	return 0;
+		Zombie *heapZombie = newZombie("heapZ");
+		heapZombie->announce();
+		delete heapZombie;
+		
+		randomChump("stackZ");
+		return 0;
+	}
+	std::cout << "write: ./exec" << std::endl;
+	return 1;
 }
