d28b0be8a873175bb9bf12789898e0bb5b6e2710
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-10-20 14:50:33 +0200

    add: little parsing for main

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/30 10:26:34 by mg                #+#    #+#             */
-/*   Updated: 2025/10/02 16:27:07 by mg               ###   ########.fr       */
+/*   Updated: 2025/10/20 14:50:28 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,23 +16,31 @@
 #include <iostream>
 
 
-int main () {
+int main (int ac, char **av) {
 
-	HumanB frank("Frank");
-	frank.attack();
+	(void)av;
 
-	Weapon club("big club");
+	if (ac == 1) {
 
-	HumanA bob("bob", club);
-	bob.attack();
-
-	frank.setWeapon(club);
-	frank.attack();
-
-	club.setType("Gun");
-	
-	bob.attack();
-	frank.attack();
-
-	return 0;
+		
+		HumanB frank("Frank");
+		frank.attack();
+		
+		Weapon club("big club");
+		
+		HumanA bob("bob", club);
+		bob.attack();
+		
+		frank.setWeapon(club);
+		frank.attack();
+		
+		club.setType("Gun");
+		
+		bob.attack();
+		frank.attack();
+		
+		return 0;
+	}
+	std::cout << "Write: ./exec" << std::endl;
+	return 1;
 }
