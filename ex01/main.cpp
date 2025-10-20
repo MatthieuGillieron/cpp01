d18346c7c626cad4e2385b084bd8a642628beb30
MatthieuGillieron <gillieron.matthieu@ik.me>
2025-10-20 14:45:42 +0200

    add: little parsing for main

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,28 +6,35 @@
 /*   By: mg <mg@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/29 14:22:03 by mg                #+#    #+#             */
-/*   Updated: 2025/09/29 15:15:14 by mg               ###   ########.fr       */
+/*   Updated: 2025/10/20 14:45:34 by mg               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
-int	main () {
-	Zombie nothing;
-	Zombie name;
+int	main (int ac, char **av) {
 
-	nothing.announce();
-	name.setName("[custom name]");
-	name.announce();
+	if (ac == 1) {
 
+		Zombie nothing;
+		Zombie name;
+		
+		nothing.announce();
+		name.setName("[custom name]");
+		name.announce();
+		
+		
+		
+		Zombie* horde = zombieHorde(3, "[Zomblar]");
+		
+		for (int i = 0; i < 3; i++)
+		horde[i].announce();
+		
+		delete[] horde;
+		
+		return 0;
+	}
 
-
-Zombie* horde = zombieHorde(3, "[Zomblar]");
-
-for (int i = 0; i < 3; i++)
-	horde[i].announce();
-
-delete[] horde;
-
-	return 0;
+	std::cout << "Write: ./exec" << std::endl;
+	return 1;
 }
